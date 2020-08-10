167d9edb660091236a7a55e08e42bf41f8aae447
choldener <choldener@users.noreply.github.com>
2020-08-10 03:56:15 -0500

    Update wall_detection.py
    Added Exception on missing image
    Decreased element size
    Added clicking script

--- wall_detection.py
+++ wall_detection.py
@@ -1,19 +1,26 @@
 import cv2 
 import pyautogui
-  
+import numpy as np
+import time
 
 
 img = cv2.imread("test.png") 
 orig_img = img.copy()
-image_coords = pyautogui.locateOnScreen(orig_img, confidence=0.9)
-#conttest = np.vstack(contours)
-#conttest[:,0,:]
+
+####################
+#IMAGE RECOG
+#
+box = pyautogui.locateOnScreen(orig_img, confidence=0.9)
+if box is None:
+    raise RuntimeError("Could not detect iamge on screen")
+#
+####################
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
   
 ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
   
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 30)) 
+rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1)) 
   
 dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1) 
 
@@ -22,6 +29,24 @@ im2 = img.copy()
 
 cv2.drawContours(im2, contours, -1, (0,255,0), 3)
 
+#############
+#Contour testing
+#
+# cv2.imshow("Wall Detection", im2)
+# cv2.waitKey(0)
+#
+#############
 
-cv2.imshow("Wall Detection", im2)
-cv2.waitKey(0)
+#############
+#Clicking Script
+#working(for the most part)
+#bug, if coords are to close to eachother roll2 may click twice in same spot
+conttest = np.vstack(contours)
+time.sleep(5)
+for c in conttest[:,0,:]:
+    x = c[0] + box[0]
+    y = c[1] + box[1]
+    pyautogui.click(x, y)   
+pyautogui.click(conttest[:,0,:][0,0] + box[0] , conttest[:,0,:][0,1]+box[1])
+#
+##############
