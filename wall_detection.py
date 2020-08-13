6d9c138f3b7ae28457b2453b59efaee351fc4ef8
choldener <choldener@users.noreply.github.com>
2020-08-13 01:44:17 -0500

    Updated

--- wall_detection.py
+++ wall_detection.py
@@ -3,51 +3,98 @@ import pyautogui
 import numpy as np
 import time
 import fire
+import scipy.spatial.distance
+from matplotlib import pyplot as plt
 
-def wall_detection(IMAGE):
+def wall_detection(IMAGE, approx = True,epsilon_value= 0.005):
     
     img = cv2.imread(IMAGE) 
-    orig_img = img.copy()
     
     ####################
     #IMAGE RECOG
     #
-    box = pyautogui.locateOnScreen(orig_img, confidence=0.9)
+    box = pyautogui.locateOnScreen(img, confidence=0.9)
     if box is None:
         raise RuntimeError("Could not detect iamge on screen")
+    print(box)
     #
     ####################
     
+    ####################
+    #Computer Vision
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
-    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1)) 
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15)) 
     dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1) 
-    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)[-2:]
     im2 = img.copy()
-    cv2.drawContours(im2, contours, -1, (0,255,0), 3)
-    
-    #############
-    #Contour testing
-    #
-    # cv2.imshow("Wall Detection", im2)
-    # cv2.waitKey(0)
-    #
-    #############
-    
-    #############
-    #Clicking Script
-    #working(for the most part)
-    #bug, if coords are to close to eachother roll2 may click twice in same spot
-    conttest = np.vstack(contours)
-    time.sleep(5)
-    for c in conttest[:,0,:]:
-        x = c[0] + box[0]
-        y = c[1] + box[1]
-        pyautogui.click(x, y)   
-    pyautogui.click(conttest[:,0,:][0,0] + box[0] , conttest[:,0,:][0,1]+box[1])
-    #
-    ##############
 
-if __name__ == "__main__":
-    fire.Fire(wall_detection)
+    #cv2.drawContours(im2, contours, -1, (0,255,0), 3)
+    #
+    ####################
+
+    ####################
+    # Testing
+    #
+    # for array in contours:
+    #     conttest = np.vstack(array)
+    #     #print(conttest)
+    #     print("SPACE")
+    #     # global distance
+    #     # for c in conttest:
+    #     #     #calculate distance
+    #     #     pass
+    #     # if distance > 10:
+    #     for c in conttest:
+    #         x = c[0] + box[0]
+    #         y = c[1] + box[1]
+    #         #pyautogui.click(x, y)
+    #         #print('x:' + str(x))
+    #         #print('y:'+str(y))
+    #         #d = scipy.spatial.distance.cdist(x,y)
+    #         #print(d)
+    #     print(conttest)
+    #     print('x_first: ' + str(conttest[0][0] + box[0]))
+    #     print('y_first:'+ str(conttest[0][1] + box[1]))
+    #
+    ####################
+    #Contour testing
+    # 
+    cv2.imshow("Wall Detection", im2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    #
+    ####################
+    
+    ####################
+    #Clicking Script
+    #
+    time.sleep(3)
+    if approx == True:
+        for array in contours:
+            epsilon = epsilon_value*cv2.arcLength(array,True)
+            approx = cv2.approxPolyDP(array,epsilon,True)
+            approx = np.vstack(approx)
+            time.sleep(1)
+            for c in approx:
+                x = c[0] + box[0]
+                y = c[1] + box[1]
+                pyautogui.click(x, y)
+            #print(conttest[0][0] + box[0] , conttest[0][1]+box[1])
+            pyautogui.click(approx[0][0] + box[0] , approx[0][1]+box[1])
+            pyautogui.click(approx[0][0] + box[0] , approx[0][1]+box[1], button='right')
+    else: 
+        for array in contours:
+            conttest = np.vstack(array)
+            for c in conttest:
+                x = c[0] + box[0]
+                y = c[1] + box[1]
+                pyautogui.click(x, y)   
+            pyautogui.click(conttest[0][0] + box[0] , conttest[0][1]+box[1])
+            pyautogui.click(conttest[0][0] + box[0] , conttest[0][1]+box[1], button='right')
+    #
+    ####################
+    
+# if __name__ == "__main__":
+#     fire.Fire(wall_detection)
     
