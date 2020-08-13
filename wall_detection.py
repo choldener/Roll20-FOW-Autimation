530b6a55c096041595b17c94a6e06ee482727fc1
choldener <choldener@users.noreply.github.com>
2020-08-13 04:30:47 -0500

    Update
    Added Failsafe
    
    Fixed nonapprox mode by encoperating point distance calulations on first index call
    
    Added parameters

--- wall_detection.py
+++ wall_detection.py
@@ -4,9 +4,8 @@ import numpy as np
 import time
 import fire
 import scipy.spatial.distance
-from matplotlib import pyplot as plt
 
-def wall_detection(IMAGE, approx = True,epsilon_value= 0.005):
+def wall_detection(IMAGE, approx = True, kernel = (15,15),epsilon_value= 0.005):
     
     img = cv2.imread(IMAGE) 
     
@@ -16,7 +15,6 @@ def wall_detection(IMAGE, approx = True,epsilon_value= 0.005):
     box = pyautogui.locateOnScreen(img, confidence=0.9)
     if box is None:
         raise RuntimeError("Could not detect iamge on screen")
-    print(box)
     #
     ####################
     
@@ -24,18 +22,18 @@ def wall_detection(IMAGE, approx = True,epsilon_value= 0.005):
     #Computer Vision
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
-    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15)) 
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel) 
     dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1) 
     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)[-2:]
     im2 = img.copy()
 
-    #cv2.drawContours(im2, contours, -1, (0,255,0), 3)
+    cv2.drawContours(im2, contours, -1, (0,255,0), 3)
     #
     ####################
 
     ####################
     # Testing
-    #
+    # 
     # for array in contours:
     #     conttest = np.vstack(array)
     #     #print(conttest)
@@ -45,17 +43,22 @@ def wall_detection(IMAGE, approx = True,epsilon_value= 0.005):
     #     #     #calculate distance
     #     #     pass
     #     # if distance > 10:
-    #     for c in conttest:
-    #         x = c[0] + box[0]
-    #         y = c[1] + box[1]
-    #         #pyautogui.click(x, y)
-    #         #print('x:' + str(x))
-    #         #print('y:'+str(y))
-    #         #d = scipy.spatial.distance.cdist(x,y)
-    #         #print(d)
-    #     print(conttest)
-    #     print('x_first: ' + str(conttest[0][0] + box[0]))
-    #     print('y_first:'+ str(conttest[0][1] + box[1]))
+    #     for index, c in enumerate(conttest):
+    #         print(c)
+    #         print(index)
+    #         if index > 0:
+    #             print(array[index - 1])
+    #             d = scipy.spatial.distance.cdist(array[index-1],array[index])
+    #             print('distance: '+ str(d))
+    #         x = c[0] #+ box[0]
+    #         y = c[1] #+ box[1]
+            #print('x:' + str(x))
+            #print('y:'+str(y))
+            #d = scipy.spatial.distance.cdist(x,y)
+            #print(d)
+        #print(conttest)
+        #print('x_first: ' + str(conttest[0][0] ))#+ box[0]))
+        #print('y_first:'+ str(conttest[0][1] ))#+ box[1]))
     #
     ####################
     #Contour testing
@@ -71,7 +74,8 @@ def wall_detection(IMAGE, approx = True,epsilon_value= 0.005):
     #
     time.sleep(3)
     if approx == True:
-        for array in contours:
+        print('approx')
+        for array, index in enumerate(contours):
             epsilon = epsilon_value*cv2.arcLength(array,True)
             approx = cv2.approxPolyDP(array,epsilon,True)
             approx = np.vstack(approx)
@@ -84,9 +88,14 @@ def wall_detection(IMAGE, approx = True,epsilon_value= 0.005):
             pyautogui.click(approx[0][0] + box[0] , approx[0][1]+box[1])
             pyautogui.click(approx[0][0] + box[0] , approx[0][1]+box[1], button='right')
     else: 
+        print("not approx")
         for array in contours:
             conttest = np.vstack(array)
-            for c in conttest:
+            for index, c in enumerate(conttest):
+                if index == 1:
+                    d = scipy.spatial.distance.cdist(array[index-1],array[index])
+                    if  d < 2.5:
+                        continue
                 x = c[0] + box[0]
                 y = c[1] + box[1]
                 pyautogui.click(x, y)   
